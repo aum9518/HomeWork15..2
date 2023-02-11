75b43584ffcd412b300e5cac4312c5cff1e66700
aum9518 <abduganiuulumanasbek@gmail.com>
2023-02-11 18:44:57 +0600

    add HomeWork15.2

--- src/Circle.java
+++ src/Circle.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class Circle {
+public class Circle {
+
+    static void area(){
+        double result = Math.PI*(5*5);
+        System.out.println("Area: "+result);
+    }
+    static void circumference(){
+        double result1 = Math.PI*2*5;
+        System.out.println("Circumference: "+result1);
+
+    }
+
 }
